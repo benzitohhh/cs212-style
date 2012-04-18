8ee0e58e73ed2dc95132545c3a8a24f27a6ed03f
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-04-18 20:52:20 -0300

    first commit

---
+++ cs212_week1_a.py
@@ -0,0 +1,15 @@
+# -----------
+# User Instructions
+# 
+# Modify the card_ranks() function so that cards with
+# rank of ten, jack, queen, king, or ace (T, J, Q, K, A)
+# are handled correctly. Do this by mapping 'T' to 10, 
+# 'J' to 11, etc...
+
+def card_ranks(cards):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
+    ranks.sort(reverse=True)
+    return ranks
+
+print card_ranks(['AC', '3D', '4S', 'KH']) #should output [14, 13, 4, 3]
