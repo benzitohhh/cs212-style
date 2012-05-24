236762bc4696e17a10a262f6af7f9499cc0341e8
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-05-23 22:00:20 -0300

    more week 6

---
+++ week6-scrabble/07_singleRow1.py
@@ -0,0 +1,15 @@
+class anchor(set):
+    """An anchor is where a new word can be placed; has a set of allowable letters"""
+
+# Unrestricted anchor (can take any letter)
+LETTERS = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+ANY = anchor(LETTERS) 
+
+# Restricted anchors
+mnx, moab = anchor('MNX'), anchor('MOAB')
+
+# so our row looks like this...
+a_row = row = ['|', 'A', mnx, moab, '.', '.', ANY, 'B', 'E', ANY, 'C', ANY, '.', ANY, 'D', ]
+
+# A hand
+a_hand = 'ABCEHKN'
